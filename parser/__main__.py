6759cda4a9f7f6b13680663b4a71267a590f1663
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-11-27 10:37:20 +0500

    Add perf_counter to measure working time.

--- parser/__main__.py
+++ parser/__main__.py
@@ -4,6 +4,6 @@ import asyncio
 from parser.main import main
 
 if __name__ == "__main__":
-    start_time = time.time()
+    start_time = time.perf_counter()
     asyncio.run(main())
-    logging.info(msg=f"Working time: {time.time() - start_time}")
+    logging.info(msg=f"Working time: {time.perf_counter() - start_time}")
